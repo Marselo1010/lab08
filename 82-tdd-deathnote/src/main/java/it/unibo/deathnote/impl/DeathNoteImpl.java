755317876aeedbe0112c945d8554959eb7ba38a3
Gianluca Marsella - gianluca.marsella@studio.unibo.it <gianluca.marsella@studio.unibo.it>
2025-11-10 13:02:39 +0100

    creato primo metodo e campi di DeathNoteImpl

---
+++ 82-tdd-deathnote/src/main/java/it/unibo/deathnote/impl/DeathNoteImpl.java
@@ -0,0 +1,68 @@
+package it.unibo.deathnote.impl;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import it.unibo.deathnote.api.DeathNote;
+
+public class DeathNoteImpl implements DeathNote{
+
+    private Map<String,CauseDetails> person = new HashMap<>();
+
+    @Override
+    public String getRule(int ruleNumber) {
+       if(ruleNumber < 0 && ruleNumber > RULES.size()){
+        throw new IllegalArgumentException("Your rule number doesn't exist -> must be 0-" + RULES.size());
+       } else {
+            return RULES.get(ruleNumber);
+       }
+    }
+
+    @Override
+    public void writeName(String name) {
+        
+    }
+
+    @Override
+    public boolean writeDeathCause(String cause) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'writeDeathCause'");
+    }
+
+    @Override
+    public boolean writeDetails(String details) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'writeDetails'");
+    }
+
+    @Override
+    public String getDeathCause(String name) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getDeathCause'");
+    }
+
+    @Override
+    public String getDeathDetails(String name) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getDeathDetails'");
+    }
+
+    @Override
+    public boolean isNameWritten(String name) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'isNameWritten'");
+    }
+
+    public static class CauseDetails{
+        
+        private String cause;
+        private String details;
+
+        CauseDetails(String cause, String details){
+            this.cause = cause;
+            this.details = details;
+
+        }
+    }
+
+}
