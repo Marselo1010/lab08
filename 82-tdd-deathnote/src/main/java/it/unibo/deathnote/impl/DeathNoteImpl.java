4ac9d5adefcd12c40bbda89981947b790091879d
marsella <gianluca.marsella@studio.unibo.it>
2025-11-13 23:24:42 +0100

    esercizio funzionante,manca da finire la build

--- 82-tdd-deathnote/src/main/java/it/unibo/deathnote/impl/DeathNoteImpl.java
+++ 82-tdd-deathnote/src/main/java/it/unibo/deathnote/impl/DeathNoteImpl.java
@@ -8,58 +8,79 @@ import it.unibo.deathnote.api.DeathNote;
 public class DeathNoteImpl implements DeathNote{
     
     private Map<String,CauseDetails> person = new HashMap<>();
+    private String lastPerson = null;
+    private long time = 0;
 
     @Override
     public String getRule(int ruleNumber) {
-       if(ruleNumber < 0 && ruleNumber > RULES.size()){
+       if(ruleNumber < 1 || ruleNumber > RULES.size()){
         throw new IllegalArgumentException("Your rule number doesn't exist -> must be [ 0-" + RULES.size() + " ]");
        } else {
-            return RULES.get(ruleNumber);
+            return RULES.get(ruleNumber-1);
        }
     }
 
     @Override
     public void writeName(String name) {
+        this.time = System.currentTimeMillis();
+        this.lastPerson = name;
         if(name == null){
             throw new NullPointerException("you must write the name of the person");
         } else if (person.containsKey(name)) {
             throw new ClassCastException("this name is already present in the Death Note");
-             } else {
-                person.put(name, null);
-             }
-
-        
-        
+            } else {
+                person.put(name, new CauseDetails("heart attack",""));
+            }
     }
 
     @Override
     public boolean writeDeathCause(String cause) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'writeDeathCause'");
+
+        if(lastPerson == null){
+            throw new IllegalStateException("first you have to write a name for the person");
+        } else if(cause == null){
+            throw new IllegalStateException("the cause can't be null");
+        }
+
+        if(System.currentTimeMillis() - this.time <= 40){
+            person.get(lastPerson).setCause(cause);
+            return true;  
+        }
+        
+        return false;
     }
 
     @Override
     public boolean writeDetails(String details) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'writeDetails'");
+
+        if(lastPerson == null){
+            throw new IllegalStateException("first you have to write a name for the person");
+        } else if(details == null){
+            throw new IllegalStateException("the cause can't be null");
+        }
+
+        if(System.currentTimeMillis() - this.time <= 6040){
+            person.get(lastPerson).setDetails(details);
+            return true;  
+        }
+        
+        return false;
+
     }
 
     @Override
     public String getDeathCause(String name) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getDeathCause'");
+       return person.get(name).getCause();
     }
 
     @Override
     public String getDeathDetails(String name) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getDeathDetails'");
+        return person.get(name).getDetails();
     }
 
     @Override
     public boolean isNameWritten(String name) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'isNameWritten'");
+       return person.containsKey(name);
     }
 
     public static class CauseDetails{
@@ -73,6 +94,10 @@ public class DeathNoteImpl implements DeathNote{
 
         }
 
+        CauseDetails(){
+
+        }
+
         //getter
 
         public String getCause() {
@@ -81,6 +106,16 @@ public class DeathNoteImpl implements DeathNote{
 
         public String getDetails() {
             return this.details;
+        }
+
+        //setter 
+
+        public void setCause(String cause){
+            this.cause = cause;
+        }
+
+        public void setDetails(String details){
+            this.details = details;
         }
     }
 
--- 82-tdd-deathnote/src/test/java/it/unibo/deathnote/TestDeathNote.java
+++ 82-tdd-deathnote/src/test/java/it/unibo/deathnote/TestDeathNote.java
@@ -3,15 +3,10 @@ package it.unibo.deathnote;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static it.unibo.deathnote.api.DeathNote.RULES;
 
-import java.util.List;
-
 import org.junit.jupiter.api.Assertions;
-
-import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -22,24 +17,17 @@ import it.unibo.deathnote.impl.DeathNoteImpl;
 class TestDeathNote {
 
     private DeathNote deathNoteTest;
-    final String person = "marco";
-    final String person2 = "giulia"; 
-    final String notWrittenPerson = "different person";
+    private static final String PERSON = "marco";
+    private static final String PERSON_2 = "giulia"; 
+    private static final String NOT_WRITTEN_PERSON = "different person";
 
     @BeforeEach
-    void setUp(){
+    void setUp() {
         deathNoteTest = new DeathNoteImpl();
-        
     }
 
-    /*
-     * 1. Rule number 0 and negative rules do not exist in the DeathNote rules.
-     * check that the exceptions are thrown correctly, that their type is the 
-     * expected one, and that the message is not null, empty, or blank.
-     */
-
     @Test
-    void testRuleNumber(){
+    void testRuleNumber() {
         try {
             deathNoteTest.getRule(0);
             Assertions.fail("rule number 0 doesn't exist");    
@@ -57,100 +45,62 @@ class TestDeathNote {
             assertFalse(e.getMessage().isEmpty()); 
         }
     }
-    /*
-     * 2. No rule is empty or null in the DeathNote rules.
-     * for all the valid rules, check that none is null or blank
-     */
-    @Test
-    void testNoEmptyOrNullRule(){
 
-        for(int i = 1; i <= RULES.size(); i++){
+    @Test
+    void testNoEmptyOrNullRule() {
+
+        for (int i = 1; i < RULES.size(); i++) {
             assertFalse(deathNoteTest.getRule(i).isEmpty());
             assertFalse(deathNoteTest.getRule(i).isBlank());
             assertNotNull(deathNoteTest.getRule(i));
         }
     }
-    /*  
-     * 3. The human whose name is written in the DeathNote will eventually die.
-      * verify that the human has not been written in the notebook yet
-      * write the human in the notebook
-      * verify that the human has been written in the notebook
-      * verify that another human has not been written in the notebook
-      * verify that the empty string has not been written in the notebook
-      */
-    @Test
-    void testWrittenPerson(){
 
-        assertFalse(deathNoteTest.isNameWritten(person));
-        deathNoteTest.writeName(person);
-        assertTrue(deathNoteTest.isNameWritten(person));
-        assertFalse(deathNoteTest.isNameWritten(notWrittenPerson));
+    @Test
+    void testWrittenPerson() {
+
+        assertFalse(deathNoteTest.isNameWritten(PERSON));
+        deathNoteTest.writeName(PERSON);
+        assertTrue(deathNoteTest.isNameWritten(PERSON));
+        assertFalse(deathNoteTest.isNameWritten(NOT_WRITTEN_PERSON));
         assertFalse(deathNoteTest.isNameWritten(""));
     }
 
-    /*
-      * 4. If the cause of death is written within the next 40 milliseconds of writing the person's name, it will happen.
-      * If the cause of death is not specified, the person will simply die of a heart attack.
-      * check that writing a cause of death before writing a name throws the correct exception
-      * write the name of a human in the notebook
-      * verify that the cause of death is a heart attack
-      * write the name of another human in the notebook
-      * set the cause of death to "karting accident"
-      * verify that the cause of death has been set correctly (returned true, and the cause is indeed "karting accident")
-      * sleep for 100ms
-      * try to change the cause of death 
-      * verify that the cause of death has not been changed
-      */
-
       @Test
-      void testCauseOfDeath() throws InterruptedException{
+      void testCauseOfDeath() throws InterruptedException {
 
             try {
                 deathNoteTest.writeDeathCause("suicide");
                 Assertions.fail("you can't write the cause before writing the name of the person");
             } catch (final IllegalStateException e) {
-                
-            }   
-        
-        deathNoteTest.writeName(person);
-        assertEquals("heart attack", deathNoteTest.getDeathCause(person));
-        deathNoteTest.writeName(person2);
+
+            }
+
+        deathNoteTest.writeName(PERSON);
+        assertEquals("heart attack", deathNoteTest.getDeathCause(PERSON));
+        deathNoteTest.writeName(PERSON_2);
         deathNoteTest.writeDeathCause("karting accident");
         Thread.sleep(100);
         deathNoteTest.writeDeathCause("car incident");
-        assertEquals("karting accident",deathNoteTest.getDeathCause(person2));
+        assertEquals("karting accident", deathNoteTest.getDeathCause(PERSON_2));
       }
 
-    /*
-      * 5. After writing the cause of death, details of the death should be written in the next 6 seconds and 40 milliseconds
-      * of writing the death's cause.
-      * check that writing the death details before writing a name throws the correct exception
-      * write the name of a human in the notebook
-      * verify that the details of the death are currently empty
-      * set the details of the death to "ran for too long"
-      * verify that death details have been set correctly (returned true, and the details are indeed "ran for too long")
-      * write the name of another human in the notebook
-      * sleep for 6100ms
-      * try to change the details
-      * verify that the details have not been changed
-      */
-
       @Test
-      void testDeathDetails() throws InterruptedException{
+      void testDeathDetails() throws InterruptedException {
             try {
                 deathNoteTest.writeDetails("death will come at 21:00 ");
                 Assertions.fail("you can't write the details of the death before writing the name of the person");
             } catch (final IllegalStateException e) {
                 
             }
-            deathNoteTest.writeName(person);
-            assertEquals("", deathNoteTest.getDeathDetails(person));
+            deathNoteTest.writeName(PERSON);
+            assertEquals("", deathNoteTest.getDeathDetails(PERSON));
             deathNoteTest.writeDetails("ran for too long");
-            assertEquals("ran for too long", deathNoteTest.getDeathDetails(person));
-            deathNoteTest.writeName(person2);
+            assertEquals("ran for too long", deathNoteTest.getDeathDetails(PERSON));
+            deathNoteTest.writeName(PERSON_2);
             Thread.sleep(6100);
             deathNoteTest.writeDetails("death will come at 21:00 ");
-            assertEquals("", deathNoteTest.getDeathDetails(person2));
+            assertEquals("", deathNoteTest.getDeathDetails(PERSON_2));
 
       }
 }
